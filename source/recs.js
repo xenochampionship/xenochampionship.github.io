26d0fd62e66d9e5c068baf203350b1d370d052d4
Xeno Championship <xenochampionship@gmail.com>
2026-04-19 04:08:18 +0100

    Create recs.js

---
+++ source/recs.js
@@ -0,0 +1,10 @@
+const dataHostUrl = "https://xenochampionship.github.io/source/data/";
+
+const records = [
+    {
+        title: "XC 2026", dates: "01/06/2026 - 30/06/2026", 
+        past: false, // If current or upcoming, set past to false
+        top3: ["TBD", "TBD", "TBD"], // 1st (Gold), 2nd (Silver), 3rd (Bronze)
+        resultsJson: dataHostUrl+"xc26.json"
+    }
+];
