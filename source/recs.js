41ec1127095d2ebbaebf131ce05dfbfbdbffa056
Xeno Championship <xenochampionship@gmail.com>
2026-04-21 07:26:43 +0100

    Update top3 values from 'TBD' to 'TBC'

--- source/recs.js
+++ source/recs.js
@@ -4,7 +4,7 @@ const records = [
     {
         title: "XC 2026", dates: "To Be Confirmed", 
         past: false, // If current or upcoming, set past to false
-        top3: ["TBD", "TBD", "TBD"], // 1st (Gold), 2nd (Silver), 3rd (Bronze)
+        top3: ["TBC", "TBC", "TBC"], // 1st (Gold), 2nd (Silver), 3rd (Bronze)
         resultsJson: dataHostUrl+"xc26.json"
     }
 ];
