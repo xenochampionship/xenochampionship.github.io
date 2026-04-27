4875316639f186692a41244ce7d2a704e14172fc
Xeno Championship <xenochampionship@gmail.com>
2026-04-27 06:06:44 +0100

    Fix end date for XC 2026 record

--- source/recs.js
+++ source/recs.js
@@ -8,7 +8,7 @@ const defaultCharity = {
 
 const records = [
     {
-        title: "XC 2026", dates: "01/08/2026 - 30/08/2026", 
+        title: "XC 2026", dates: "01/08/2026 - 31/08/2026", 
         past: false, // If current or upcoming, set past to false
         top3: ["TBC", "TBC", "TBC"], // 1st (Gold), 2nd (Silver), 3rd (Bronze)
         resultsJson: dataHostUrl+"xc26.json"
