ecd5403989fddcf713a3cad03af992a7e7a92de3
Xeno Championship <xenochampionship@gmail.com>
2026-05-04 17:17:07 +0100

    Update recs.js

--- source/recs.js
+++ source/recs.js
@@ -11,6 +11,6 @@ const records = [
         past: false, // If current or upcoming, set past to false
         top3: ["TBC", "TBC", "TBC"], // 1st (Gold), 2nd (Silver), 3rd (Bronze)
         resultsJson: dataHostUrl+"xc26.json",
-        charity: {current: 120.00, goal: 200.00, showOnHome: true} // GBP (£)
+        charity: {current: 160.00, goal: 200.00, showOnHome: true} // GBP (£)
     }
 ];
